19e434d9ed45d0aa32c8470ef149bf1aeb6c518c
carlosyael <62651008+carlosyael@users.noreply.github.com>
2022-06-07 17:42:12 -0400

    sdsd
    sd

---
+++ Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokedex.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Pokmaint()
+        {
+            return View();
+        }
+    }
+}
